c1579e70d4f85bd58d085cc0f1d5c1a2a7a44a03
yhaelJ <jyhael97@gmail.com>
2020-07-29 17:27:22 -0500

    Add files via upload

---
+++ run_analysis.R
@@ -0,0 +1,44 @@
+# Archivo run_analysis.R
+
+# Merge
+# train data
+x_train <- read.table(paste(sep = "", dataDir, "/UCI HAR Dataset/train/X_train.txt"))
+y_train <- read.table(paste(sep = "", dataDir, "/UCI HAR Dataset/train/Y_train.txt"))
+s_train <- read.table(paste(sep = "", dataDir, "/UCI HAR Dataset/train/subject_train.txt"))
+
+# test data
+x_test <- read.table(paste(sep = "", dataDir, "/UCI HAR Dataset/test/X_test.txt"))
+y_test <- read.table(paste(sep = "", dataDir, "/UCI HAR Dataset/test/Y_test.txt"))
+s_test <- read.table(paste(sep = "", dataDir, "/UCI HAR Dataset/test/subject_test.txt"))
+
+# merge {train, test} data
+x_data <- rbind(x_train, x_test)
+y_data <- rbind(y_train, y_test)
+s_data <- rbind(s_train, s_test)
+
+
+feature <- read.table(paste(sep = "", dataDir, "/UCI HAR Dataset/features.txt"))
+a_label <- read.table(paste(sep = "", dataDir, "/UCI HAR Dataset/activity_labels.txt"))
+a_label[,2] <- as.character(a_label[,2])
+
+selectedCols <- grep("-(mean|std).*", as.character(feature[,2]))
+selectedColNames <- feature[selectedCols, 2]
+selectedColNames <- gsub("-mean", "Mean", selectedColNames)
+selectedColNames <- gsub("-std", "Std", selectedColNames)
+selectedColNames <- gsub("[-()]", "", selectedColNames)
+
+
+# extract data por columnas usando el nombre
+x_data <- x_data[selectedCols]
+allData <- cbind(s_data, y_data, x_data)
+colnames(allData) <- c("Subject", "Actividad", selectedColNames)
+
+allData$Activity <- factor(allData$Activity, levels = a_label[,1], labels = a_label[,2])
+allData$Subject <- as.factor(allData$Subject)
+
+
+# nuevos tidy data set
+meltedData <- melt(allData, id = c("Subject", "Actividad"))
+tidyData <- dcast(meltedData, Subject + Actividad ~ variable, mean)
+
+write.table(tidyData, "./tidy_dataset.txt", row.names = FALSE, quote = FALSE)
